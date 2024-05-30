bed03b96fc4ae43db60693b3e2401f41a1d338a6
Gurveer Singh <singhgurveer337@gmail.com>
2024-05-30 18:26:54 +0530

    Added localstorage to add and product in the cart even after refreshing

--- data/cart.js
+++ data/cart.js
@@ -1,46 +1,56 @@
-export let cart = [
-    //We are not using saving images and name , i.e bcoz we are taking product id and we can search rest of the deailts in the product array.
-    //This method is called deduplicating the data or normalizing the data.
-    //this is common technique in software engineering.
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+if (!cart) {
+  //We are not using saving images and name , i.e bcoz we are taking product id and we can search rest of the deailts in the product array.
+  //This method is called deduplicating the data or normalizing the data.
+  //this is common technique in software engineering.
+  cart = [
     {
-        productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity: 2
-    },{
-        productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity: 1
-    }
-];
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1
+    }];
+}
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart)); //using JSON stringify because localstorage takes strings
+}
 
 export function addToCart(productId) {
-    const quantitySelector = document.querySelector(
-      `.js-quantity-selector-${productId}`
-    );
-    const quantity = Number(quantitySelector.value);
-    let matchingItem;
-    cart.forEach((cartItem) => {
-      if (productId === cartItem.productId) {
-        matchingItem = cartItem;
-      }
-    });
-  
-    if (matchingItem) {
-      matchingItem.quantity += quantity;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: quantity,
-      });
+  const quantitySelector = document.querySelector(
+    `.js-quantity-selector-${productId}`
+  );
+  const quantity = Number(quantitySelector.value);
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
     }
-  }
+  });
 
-  export function removeFromCart(productId){
-    let newCart = [];
-
-    cart.forEach((cartItem)=>{
-      if(cartItem.productId !== productId){
-        newCart.push(cartItem);
-      }
+  if (matchingItem) {
+    matchingItem.quantity += quantity;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: quantity,
     });
-
-    cart = newCart;
   }
+  saveToStorage();
+}
+
+export function removeFromCart(productId) {
+  let newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+  saveToStorage();
+}

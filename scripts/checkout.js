31b5a7cd2c838b9b54352503019615f3d600def6
Gurveer Singh <singhgurveer337@gmail.com>
2024-05-30 15:00:32 +0530

    Made utility for money and fixed radio

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
-
+import { formatCurrency } from "./utils/money.js";  
 let cartSummaryHTML = " ";
 
 cart.forEach((cartItem) => {
@@ -30,7 +30,7 @@ cartSummaryHTML += `
             ${matchingItem.name}
         </div>
         <div class="product-price">
-            $${(matchingItem.priceCents / 100).toFixed(2)}
+            $${formatCurrency(matchingItem.priceCents)}
         </div>
         <div class="product-quantity">
             <span>
@@ -52,7 +52,7 @@ cartSummaryHTML += `
         <div class="delivery-option">
             <input type="radio" checked
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${matchingItem.id}">
             <div>
             <div class="delivery-option-date">
                 Tuesday, June 21
@@ -65,7 +65,7 @@ cartSummaryHTML += `
         <div class="delivery-option">
             <input type="radio"
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${matchingItem.id}">
             <div>
             <div class="delivery-option-date">
                 Wednesday, June 15
@@ -78,7 +78,7 @@ cartSummaryHTML += `
         <div class="delivery-option">
             <input type="radio"
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${matchingItem.id}">
             <div>
             <div class="delivery-option-date">
                 Monday, June 13

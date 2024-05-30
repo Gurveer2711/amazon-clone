8650a295523829c8621eb40d8ac0fc31fc82ee29
Gurveer Singh <singhgurveer337@gmail.com>
2024-05-30 14:26:05 +0530

    generate html for order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,15 @@
-export const cart = [];
+export const cart = [
+    //We are not using saving images and name , i.e bcoz we are taking product id and we can search rest of the deailts in the product array.
+    //This method is called deduplicating the data or normalizing the data.
+    //this is common technique in software engineering.
+    {
+        productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2
+    },{
+        productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1
+    }
+];
 
 export function addToCart(productId) {
     const quantitySelector = document.querySelector(
